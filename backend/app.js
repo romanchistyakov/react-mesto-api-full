f4dc636b12e57e82153abd2d8b10c4555c17e909
Роман Чистяков <roman.chistiakov@gmail.com>
2022-10-12 01:03:03 +0200

    project 15 hotfix 6

--- backend/app.js
+++ backend/app.js
@@ -29,6 +29,12 @@ app.use(cors);
 app.use('/users', auth, users);
 app.use('/cards', auth, cards);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
